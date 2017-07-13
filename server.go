c2474b757f26420d91ea1191a8fb71080fc3e274
Gabriel Pedro <gpedro831@gmail.com>
2017-07-13 16:12:36 -0400

    [feat] adding image renderer api

--- server.go
+++ server.go
@@ -4,8 +4,14 @@ import (
 	"bytes"
 	"encoding/json"
 	"fmt"
+	"github.com/fogleman/gg"
+	"google.golang.org/appengine"
+	"google.golang.org/appengine/urlfetch"
 	"html/template"
+	"image"
+	"io"
 	"io/ioutil"
+	"log"
 	"math/rand"
 	"net/http"
 	"path"
@@ -25,6 +31,7 @@ func getRandomFromFile(file string) string {
 	return string(list[rand.Intn(len(list))])
 }
 
+// NewRandomXingamento 0800
 func NewRandomXingamento() Xingamento {
 	return Xingamento{
 		Value: fmt.Sprintf("%s %s",
@@ -32,6 +39,40 @@ func NewRandomXingamento() Xingamento {
 			getRandomFromFile("data/predicates.txt"),
 		),
 	}
+}
+
+// NewRandomXingamentoImage 0800
+func NewRandomXingamentoImage(client *http.Client) io.Reader {
+	context := gg.NewContextForImage(getPlaceholder(client))
+	const width = 1024
+	const height = 768
+
+	// TODO: improve better loading fontface
+	if err := context.LoadFontFace("/Library/Fonts/Arial.ttf", 96); err != nil {
+		panic(err)
+	}
+
+	context.SetRGB(0, 0, 0)
+	xingamento := NewRandomXingamento().Value
+	strokeSize := 6
+	for dy := -strokeSize; dy <= strokeSize; dy++ {
+		for dx := -strokeSize; dx <= strokeSize; dx++ {
+			if dx*dx+dy*dy >= strokeSize*strokeSize {
+				continue
+			}
+			x := width/2 + float64(dx)
+			y := height/2 + float64(dy)
+			context.DrawStringWrapped(xingamento, x, y, 0.5, 0.5, width/2, 1.5, gg.AlignCenter)
+		}
+	}
+	context.SetRGB(1, 1, 1)
+	context.DrawStringWrapped(xingamento, width/2, height/2, 0.5, 0.5, width/2, 1.5, gg.AlignCenter)
+
+	buff := new(bytes.Buffer)
+	context.EncodePNG(buff)
+
+	reader := bytes.NewReader(buff.Bytes())
+	return reader
 }
 
 func apiHandler(w http.ResponseWriter, r *http.Request) {
@@ -67,11 +108,43 @@ func indexHandler(w http.ResponseWriter, r *http.Request) {
 	tpl.Execute(w, data)
 }
 
+func imageHandler(w http.ResponseWriter, r *http.Request) {
+	ctx := appengine.NewContext(r)
+	client := urlfetch.Client(ctx)
+
+	xingamento := NewRandomXingamentoImage(client)
+	w.Header().Set("Content-Type", "image/png")
+
+	response, _ := ioutil.ReadAll(xingamento)
+	w.Write(response)
+}
+
 var packageDirectory string
 
 func init() {
 	_, packageDirectory, _, _ = runtime.Caller(0)
 	http.HandleFunc("/api", apiHandler)
 	http.HandleFunc("/slack", slackHandler)
+	http.HandleFunc("/image", imageHandler)
 	http.HandleFunc("/", indexHandler)
+}
+
+func getPlaceholder(client *http.Client) image.Image {
+	placeholder := "https://placeimg.com/1024/768/nature"
+	requester := http.Get
+	if client != nil {
+		requester = client.Get
+	}
+
+	response, err := requester(placeholder)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer response.Body.Close()
+
+	var wallpaper image.Image
+	wallpaper, _, err = image.Decode(response.Body)
+
+	return wallpaper
 }
