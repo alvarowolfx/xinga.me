1ae4b642434c6c2ab26bee32a6f6287e32d88aac
Alvaro Viebrantz <alvarowolfx@gmail.com>
2017-07-13 23:47:42 -0400

    [feat] Better performance generating images

--- server.go
+++ server.go
@@ -4,19 +4,26 @@ import (
 	"bytes"
 	"encoding/json"
 	"fmt"
-	"github.com/fogleman/gg"
-	"google.golang.org/appengine"
-	"google.golang.org/appengine/urlfetch"
 	"html/template"
 	"image"
+	"image/jpeg"
 	"io"
 	"io/ioutil"
 	"log"
 	"math/rand"
 	"net/http"
-	"path"
-	"runtime"
+	"strings"
 	"time"
+
+	"github.com/fogleman/gg"
+	"golang.org/x/image/font"
+	"google.golang.org/appengine"
+	"google.golang.org/appengine/urlfetch"
+)
+
+var (
+	fontSize  float64 = 42
+	imageFont font.Face
 )
 
 // Xingamento 0800
@@ -24,11 +31,9 @@ type Xingamento struct {
 	Value string `json:"xingamento"`
 }
 
-func getRandomFromFile(file string) string {
-	rand.Seed(time.Now().UnixNano())
-	all, _ := ioutil.ReadFile(path.Join(path.Dir(packageDirectory), file))
-	list := bytes.Split(all, []byte("\n"))
-	return string(list[rand.Intn(len(list))])
+type slackResponse struct {
+	ResponseType string `json:"response_type"`
+	Text         string `json:"text"`
 }
 
 // NewRandomXingamento 0800
@@ -43,23 +48,22 @@ func NewRandomXingamento() Xingamento {
 
 // NewRandomXingamentoImage 0800
 func NewRandomXingamentoImage(client *http.Client) io.Reader {
-	context := gg.NewContextForImage(getPlaceholder(client))
-	const width = 1024
-	const height = 768
+	const width = 1024 / 2
+	const height = 768 / 2
+	image := getPlaceholder(client, width, height)
 
-	// TODO: improve better loading fontface
-	if err := context.LoadFontFace("/Library/Fonts/Arial.ttf", 96); err != nil {
-		panic(err)
-	}
+	context := gg.NewContextForImage(image)
+	context.SetFontFace(imageFont)
 
 	context.SetRGB(0, 0, 0)
-	xingamento := NewRandomXingamento().Value
-	strokeSize := 6
+	xingamento := strings.ToUpper(NewRandomXingamento().Value)
+	strokeSize := 3
 	for dy := -strokeSize; dy <= strokeSize; dy++ {
 		for dx := -strokeSize; dx <= strokeSize; dx++ {
 			if dx*dx+dy*dy >= strokeSize*strokeSize {
 				continue
 			}
+
 			x := width/2 + float64(dx)
 			y := height/2 + float64(dy)
 			context.DrawStringWrapped(xingamento, x, y, 0.5, 0.5, width/2, 1.5, gg.AlignCenter)
@@ -69,21 +73,52 @@ func NewRandomXingamentoImage(client *http.Client) io.Reader {
 	context.DrawStringWrapped(xingamento, width/2, height/2, 0.5, 0.5, width/2, 1.5, gg.AlignCenter)
 
 	buff := new(bytes.Buffer)
-	context.EncodePNG(buff)
+	imgOut := context.Image()
+	jpeg.Encode(buff, imgOut, &jpeg.Options{Quality: jpeg.DefaultQuality})
 
-	reader := bytes.NewReader(buff.Bytes())
-	return reader
+	return buff
+}
+
+func getPlaceholder(client *http.Client, width, height int) image.Image {
+	placeholder := fmt.Sprintf("https://placeimg.com/%d/%d/nature", width, height)
+	requester := http.Get
+	if client != nil {
+		requester = client.Get
+	}
+
+	response, err := requester(placeholder)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer response.Body.Close()
+
+	var wallpaper image.Image
+	wallpaper, _, err = image.Decode(response.Body)
+
+	return wallpaper
+}
+
+func getRandomFromFile(file string) string {
+	rand.Seed(time.Now().UnixNano())
+	all, _ := ioutil.ReadFile(file)
+	list := bytes.Split(all, []byte("\n"))
+	return string(list[rand.Intn(len(list))])
+}
+
+func loadFontFace() {
+	// TODO: improve better loading fontface
+	var err error
+	imageFont, err = gg.LoadFontFace("fonts/Impact.ttf", fontSize)
+	if err != nil {
+		panic(err)
+	}
 }
 
 func apiHandler(w http.ResponseWriter, r *http.Request) {
 	xingamento, _ := json.Marshal(NewRandomXingamento())
 	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
 	w.Write(xingamento)
-}
-
-type slackResponse struct {
-	ResponseType string `json:"response_type"`
-	Text         string `json:"text"`
 }
 
 func slackHandler(w http.ResponseWriter, r *http.Request) {
@@ -112,39 +147,18 @@ func imageHandler(w http.ResponseWriter, r *http.Request) {
 	ctx := appengine.NewContext(r)
 	client := urlfetch.Client(ctx)
 
-	xingamento := NewRandomXingamentoImage(client)
-	w.Header().Set("Content-Type", "image/png")
+	imageReader := NewRandomXingamentoImage(client)
+	w.Header().Set("Content-Type", "image/jpeg")
 
-	response, _ := ioutil.ReadAll(xingamento)
-	w.Write(response)
+	io.Copy(w, imageReader)
 }
 
-var packageDirectory string
-
 func init() {
-	_, packageDirectory, _, _ = runtime.Caller(0)
+
+	loadFontFace()
+
 	http.HandleFunc("/api", apiHandler)
 	http.HandleFunc("/slack", slackHandler)
 	http.HandleFunc("/image", imageHandler)
 	http.HandleFunc("/", indexHandler)
-}
-
-func getPlaceholder(client *http.Client) image.Image {
-	placeholder := "https://placeimg.com/1024/768/nature"
-	requester := http.Get
-	if client != nil {
-		requester = client.Get
-	}
-
-	response, err := requester(placeholder)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	defer response.Body.Close()
-
-	var wallpaper image.Image
-	wallpaper, _, err = image.Decode(response.Body)
-
-	return wallpaper
 }
